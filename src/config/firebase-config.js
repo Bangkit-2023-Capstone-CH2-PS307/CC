3a1186d7f768267ca561fe9547b5a43051f4e0a0
Waya <wayaragil66@gmail.com>
2023-12-01 03:45:03 +0700

    refactor: update firebase config

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,11 +1,9 @@
 import admin from 'firebase-admin';
 
 // Fetch the service account key JSON file contents
-const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+// const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
-// Initialize the app with a service account, granting admin privileges
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// Initialize the app, granting admin privileges
+admin.initializeApp();
 
 export default admin;
