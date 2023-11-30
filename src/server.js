65076f13297e0f1100b6b78cd57bb8ed831ef082
Waya <wayaragil66@gmail.com>
2023-12-01 02:30:56 +0700

    debug

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
-import verifyFirebaseToken from './middleware/verifyFirebaseToken.js';
-import authRoutes from './routes/authRoutes.js';
+// import verifyFirebaseToken from './middleware/verifyFirebaseToken.js';
+// import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
