b0e11f7c05e6cf64fb9037299fe1adf17656a2e0
Waya <wayaragil66@gmail.com>
2023-12-05 23:07:02 +0700

    feat: add API index information

--- src/server.js
+++ src/server.js
@@ -11,6 +11,13 @@ app.use(express.urlencoded());
 app.use(morgan('dev'));
 
 // routes
+app.get('/api/v1/', (req, res) => {
+  res.json({
+    appName: 'NutriKita-API',
+    apiVersion: 'v1',
+    description: 'API to serve request from NutriKita Android Apps',
+  });
+});
 app.use('/api/v1/auth', authRoutes);
 
 export default app;
